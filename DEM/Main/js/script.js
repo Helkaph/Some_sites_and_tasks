c2cafba37db1a004c38b7ad9b6219f42560156c1
Helkaph <113966410+Helkaph@users.noreply.github.com>
2023-05-11 10:41:08 +0300

    Add files via upload
    Full complete project.

---
+++ DEM/Main/js/script.js
@@ -0,0 +1,44 @@
+let Keter = document.querySelector('#Keter')
+let Hohkmah = document.querySelector('#Hohkmah')
+let Binah = document.querySelector('#Binah')
+let Hesed = document.querySelector('#Hesed')
+let Geburah = document.querySelector('#Geburah')
+let Tiphereth = document.querySelector('#Tiphereth')
+let Netzach = document.querySelector('#Netzach')
+let Hod = document.querySelector('#Hod')
+let Yesod = document.querySelector('#Yesod')
+let Malkuth = document.querySelector('#Malkuth');
+
+Keter.addEventListener('click', () => {
+    window.location.href = '../Spirits/Angel/index.html'
+})
+Hohkmah.addEventListener('click', () => {
+    window.location.href = '../Spirits/Sister/index.html'
+})
+Binah.addEventListener('click', () => {
+    window.location.href = '../Spirits/Nightmare/index.html'
+})
+Hesed.addEventListener('click', () => {
+    window.location.href = '../Spirits/Hermit/index.html'
+})
+Geburah.addEventListener('click', () => {
+    window.location.href = '../Spirits/Ifreet/index.html'
+})
+Tiphereth.addEventListener('click', () => {
+    window.location.href = '../Spirits/Zodiac/index.html'
+})
+Netzach.addEventListener('click', () => {
+    window.location.href = '../Spirits/Witch/index.html'
+})
+Hod.addEventListener('click', () => {
+    window.location.href = '../Spirits/Berserk/index.html'
+})
+Yesod.addEventListener('click', () => {
+    window.location.href = '../Spirits/Diva/index.html'
+})
+Malkuth.addEventListener('click', () => {
+    window.location.href = '../Spirits/Princess/index.html'
+})
+function Spirit_Information() {
+    
+}
---
+++ DEM/Src/Spirits_Data/Zodiac.js
@@ -0,0 +1,15 @@
+let power = <table><tr>
+    <th>Сила</th>
+    <th>Защита</th>
+    <th>Духовная энергия</th>
+    <th>Ловкость</th>
+    <th>Интеллект</th>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+    </table>
